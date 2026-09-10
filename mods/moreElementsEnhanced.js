77e0b16186e10855960b9cb89a8503de2052ca77
Pixel Does Planes <94863544+pixel-doez-planes@users.noreply.github.com>
2026-09-10 08:21:57 -0700

    Create moreElementsEnhanced.js

---
+++ mods/moreElementsEnhanced.js
@@ -0,0 +1,11 @@
+// Version v0.1
+
+console.log("Build Number 1.0")
+
+// Plutonium
+elements.plutonium = {
+  colorOnDemand: function() {
+        return shuffledCleanHex("#46bd44", 15); // 15% brightness variation
+  },
+  behaviour:\
+},
