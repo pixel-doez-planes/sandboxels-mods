a19681cb206b278201bece02ca2d2b091ca0b3cd
Pixel Does Planes <94863544+pixel-doez-planes@users.noreply.github.com>
2026-09-10 10:36:27 -0700

    Update moreElementsEnhanced.js

--- mods/moreElementsEnhanced.js
+++ mods/moreElementsEnhanced.js
@@ -35,18 +35,18 @@ elements.plutonium = {
 
 
 // Turbine
-/* elements.turbine = { 
-    color: "#00ffff", 
-    behavior: behaviors.WALL, 
-    category: "machines", 
-    conduct: 1, 
-    state: "solid", 
+elements.turbine = { 
+    color: "#00ffff",
+    behavior: behaviors.WALL,
+    category: "machines",
+    conduct: 1,
+    state: "solid",
     reactions: { 
         "steam": {
             elem1: "turbine", // Turbine stays a turbine
             charge1: 1,       // Applies electrical charge to the turbine [1.13]
             elem2: "steam"    // Steam stays steam
-        }
-    } 
+        },
+    }, 
 };
-*/
+
