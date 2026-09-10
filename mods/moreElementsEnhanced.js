dc063ea57d6d8fbaafe385020ee322680adf1ee5
Pixel Does Planes <94863544+pixel-doez-planes@users.noreply.github.com>
2026-09-10 12:19:57 -0700

    Update moreElementsEnhanced.js

--- mods/moreElementsEnhanced.js
+++ mods/moreElementsEnhanced.js
@@ -3,7 +3,9 @@ console.log("Build Number 1.0");
 
 // Plutonium
 if (!elements) var elements = {};
-
+function plutoniumTick(pixel) {
+    pixel.temp = pixel.temp + 1
+}
 elements.plutonium = {
     colorOnDemand: function() { 
         return shuffledHexColor("#46bd44", 15); 
@@ -12,13 +14,14 @@ elements.plutonium = {
     behavior: [ 
         "XX|CR:fire%1 AND CR:neutron%0.5 AND CR:radiation%1|XX", 
         "M2 AND CR:neutron%0.5 AND CR:radiation%1|CH:uranium%0.01|M2 AND CR:neutron%0.5 AND CR:radiation%1", 
-        "M1|M1|M1" 
+        "M1|M1|M1"
     ],
-    category: "powders",
-    state: "powder",
+    category: "solid",
+    state: "solid",
+    tick: (pixel) => plutoniumTick()
     temp: 20,
     // Heating properties (Explodes at 100°C) 
-    tempHigh: 100,
+    tempHigh: 3000,
     stateHigh: "big_explosion",
     // Reactions object handling both Potassium and Neutrons 
     reactions: { 
@@ -29,8 +32,8 @@ elements.plutonium = {
         "neutron": { 
             temp1: 9, // Adds 9°C to the plutonium pixel per hit!
             elem2: null // Absorbs/deletes the neutron pixel 
-        }, 
-    }, 
+        },
+    },
 },
 
 
