03e1b67b3719a178ab3ac10ba9ca93985cfd1bea
Pixel Does Planes <94863544+pixel-doez-planes@users.noreply.github.com>
2026-09-09 17:13:47 -0700

    Create nitroglycerin.js

---
+++ mods/nitroglycerin.js
@@ -0,0 +1,27 @@
+// To create a mod:
+// Create a new Javascript file like this one.
+// Add the file to the mods folder on GitHub, or host it somewhere else.
+// https://github.com/R74nCom/sandboxels/tree/main/mods
+
+// To learn about modding, check the wiki: https://sandboxels.wiki.gg/wiki/Modding
+// Or join our Discord: https://r74n.com/discord/
+
+// To add it in the Mod Manager:
+// If it is in the mods folder, you can just use the name of the file. (example_mod.js)
+// If it is hosted somewhere else, you can use the full URL, including the HTTPS://.
+
+// Adding elements:
+elements.glycerin = {
+    color: "#ffff00",
+    behavior: behaviors.POWDER,
+    category: "powder",
+    state: "powder",
+    density: 1100,
+    reactions: {
+        "potassium": { elem1:"explosion", elem2:null, chance:1 },
+    },
+    viscosity: 60000,
+}
+
+// Removing elements:
+// Be aware, things may break
