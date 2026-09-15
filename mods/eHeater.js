bd54c81b57e65f0d6d7c03d66989d7a5f64cde2b
Pixel Does Planes <94863544+pixel-doez-planes@users.noreply.github.com>
2026-09-15 08:22:38 -0700

    Create eHeater.js

---
+++ mods/eHeater.js
@@ -0,0 +1,12 @@
+elements.e_heater = {
+    color: "#b22222", // Firebrick red color
+    behavior: [
+        "XX|HT:5|XX",
+        "HT:5|XX|HT:5",
+        "XX|HT:5|XX"
+    ],
+    category: "machines",
+    state: "solid",
+    temp: 50,
+    conduct: 1
+};
